8e778686d0c4de922c0196cf24dd4fc77283a01c
mwilliams455 <m.williams.455@gmail.com>
2026-09-10 06:26:31 +0100

    apk1a: label REALRAW1A diagnostic build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.m11.diagnostic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0-apk1a-openidentify"
+        versionCode = 2
+        versionName = "0.1.1-apk1a-realraw1a"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
 
